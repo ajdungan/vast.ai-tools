2cc0f3845e8561ae5d614523ed8753bd95a76175
Etherion <19214485+jjziets@users.noreply.github.com>
2021-08-27 12:12:10 +0200

    Update run_client.sh

--- uptime-monitor/run_client.sh
+++ uptime-monitor/run_client.sh
@@ -5,11 +5,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/.env
 
 WORKER=$1
-
+PUBIP="$(curl ifconfig.me.)"
+PUBIP = echo "$PUBIP//.}"
 if ! [ -n "$WORKER" ]; then
   WORKER="unknown-worker"
 fi
-
+WORKER="$PUBIP-$WORKER"
+echo $WORKER
 while [ 1 ]
 do
   request_url="http://$SERVER_ADDR:$SERVER_PORT/ping/$WORKER?api_key=$API_KEY"
@@ -18,3 +20,4 @@ do
   curl -m 2 "$request_url"
   sleep $PING_INTERVAL
 done
+
