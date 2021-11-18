195a63b9704be0bedbe681986bf87b89b9c212e0
Etherion <19214485+jjziets@users.noreply.github.com>
2021-11-18 13:52:07 +0200

    Update event.py

--- analytics/client/crons/event.py
+++ analytics/client/crons/event.py
@@ -1,27 +1,25 @@
 from lib.database import Database
 import time
 import settings
+from lib.sys import log
 
 def run():
     db = Database()
 
-    values = [None, None, None, None]
+    values = []
 
     for key, item in enumerate(settings.args.values.split(",")):
         if len(item) > 45:
             item = item[0:44]
 
-        if key not in values:
-            log("WARNING - Key:", key, "not in values:", values)
-            continue
-
-        values[key] = item
+        values.append(item)
 
     db.insert_event({
         "time": settings.args.time,
         "name": settings.args.name,
-        "val1": values[0],
-        "val2": values[1],
-        "val3": values[2],
-        "val4": values[3],
+        "val1": values[0] if len(values) >= 1 else None,
+        "val2": values[1] if len(values) >= 2 else None,
+        "val3": values[2] if len(values) >= 3 else None,
+        "val4": values[3] if len(values) >= 4 else None
     })
+
