d3951a0142157cc4fea9dd2f1b31cb7a6ce77fd7
Etherion <19214485+jjziets@users.noreply.github.com>
2021-11-24 12:24:23 +0200

    Update settings.py

--- analytics/client/settings.py
+++ analytics/client/settings.py
@@ -9,10 +9,10 @@ parser.add_argument('--time')
 parser.add_argument('--values')
 args = parser.parse_args()
 
-
 VAST_MACHINE_ID = int(os.getenv("VAST_MACHINE_ID"))
 VAST_USERNAME = os.getenv("VAST_USERNAME")
 VAST_PASSWORD = os.getenv("VAST_PASSWORD")
+VAST_API_KEY = os.getenv("VAST_API_KEY")
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
